45f45b4f9cd42b25623023f03b170e73cb0d2dc1
Pin-Wang <1228935432@qq.com>
2016-08-11 14:22:24 +0800

    fd

--- src/设计模式/SingleTon.java
+++ src/设计模式/SingleTon.java
@@ -10,13 +10,34 @@ public class SingleTon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Single_d s1=Single_d.getInstance();
+		Single_d s2=Single_d.getInstance();
+		System.out.println(s1==s2);
 	}
 
 }
 
-//单例模式
 
+//单例模式（双重锁检查）
+class Single_d{
+	private static Single_d Instance;
+	private static Object object=new Object();
+	//构造器私有化
+	private Single_d(){}
+	//静态公开方法返回实例
+	public static Single_d getInstance(){
+		if(Instance==null){
+			synchronized(object){
+				if(Instance==null){
+					Instance=new Single_d();
+				}
+			}
+		}
+		return Instance;
+	}
+}
+
+//单例模式(内部类实现)
 class Single{
 	//将构造器变为私有
 	private Single(){
